082d7f66ae1b3e68f73bfd8d829c51a6f9c28306
vimaksi <victoria_maksimova@mail.ru>
2025-07-09 00:44:37 +0300

    Новый коммит

---
+++ src/main/java/Car.java
@@ -0,0 +1,10 @@
+public class Car {
+    String name; //название авто
+    int speed; //скорость
+
+    Car(String name, int speed){// конструктор
+        this.name = name;
+        this.speed = speed;
+    }
+}
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,45 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        String leader = "";
+
+        for (int i = 0; i < 3; i ++) {
+            String carName;
+            int carSpeed;
+            int minSpeed = 1;
+            int maxSpeed = 250;
+
+            while (true) {
+                System.out.println("— Введите название машины №" + (i + 1) + ":");
+              //  if (scanner.hasNextLine()) {
+                    carName = scanner.nextLine();
+                    if (carName.trim().isEmpty()) {
+                        System.out.println("Неправильное название");
+                    } else {
+                        break;
+               //     }
+                }
+            }
+
+            while (true) {
+                System.out.println("— Введите скорость машины №" + (i + 1) + ":");
+                {
+                    try {
+                        carSpeed = Integer.parseInt(scanner.nextLine());
+                        if ((carSpeed >= minSpeed) && (carSpeed <= maxSpeed)) {
+                            break;
+                        }
+                    } catch (NumberFormatException ex) {
+                    }
+                    System.out.println("— Неправильная скорость ");
+                }
+            }
+            Car car = new Car(carName, carSpeed);
+
+            leader = Race.findLeader(car.name, car.speed);
+        }
+        System.out.println("Самая быстрая машина:" + leader);
     }
 }
